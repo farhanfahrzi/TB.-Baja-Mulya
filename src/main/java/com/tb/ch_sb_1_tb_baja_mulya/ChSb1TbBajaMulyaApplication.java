5004f26f401b9116e71e15674296dc9d237e9426
farhan <ffahrezi56@gmail.com>
2024-08-31 09:20:20 +0700

    Initial Commit

---
+++ src/main/java/com/tb/ch_sb_1_tb_baja_mulya/ChSb1TbBajaMulyaApplication.java
@@ -0,0 +1,13 @@
+package com.tb.ch_sb_1_tb_baja_mulya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChSb1TbBajaMulyaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChSb1TbBajaMulyaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tb/ch_sb_1_tb_baja_mulya/ChSb1TbBajaMulyaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tb.ch_sb_1_tb_baja_mulya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChSb1TbBajaMulyaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
